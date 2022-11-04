df2a11a470a92145c7fc77546e209dc3f87028b3
Aylin <92411850+aylinhyusmen@users.noreply.github.com>
2022-11-04 22:09:40 +0200

    Added new features to the map. Added favorites to the home screen.

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -3,13 +3,14 @@ import React from 'react'
 import tw from 'twrnc'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setDestination } from '../slices/navSlice'
 import { useNavigation } from '@react-navigation/native'
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
     const navigation = useNavigation();
+    
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Good Morning, Aylin</Text>
