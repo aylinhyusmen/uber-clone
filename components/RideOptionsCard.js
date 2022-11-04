6765cdf0063e2748717f1e2e162826bce96073bd
Aylin <92411850+aylinhyusmen@users.noreply.github.com>
2022-11-05 01:01:43 +0200

    Travel time, distance, price - calculations. Home button. +

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -3,6 +3,8 @@ import tw from "twrnc"
 import { Icon } from '@rneui/base'
 import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { selectTravelTimeInformation } from '../slices/navSlice'
 
 const data = [
   {
@@ -25,9 +27,12 @@ const data = [
 },
 ]
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
@@ -38,7 +43,7 @@ const RideOptionsCard = () => {
 
           <Icon name="chevron-left" type="fontawesome" />
         </TouchableOpacity>
-      <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      <Text style={tw`text-center py-5 text-xl`}>Select a Ride - {travelTimeInformation?.distance?.text}</Text>
       </View >
 
       <FlatList 
@@ -56,13 +61,17 @@ const RideOptionsCard = () => {
             />
             <View  style={tw`-ml-6`}>
               <Text style={tw`text-xl font-semibold`}>{title}</Text>
-              <Text>Travel time...</Text>
+              <Text>duration {travelTimeInformation?.duration?.text}</Text>
             </View>
-            <Text style={tw`text-xl `}>99€</Text>
+            <Text style={tw`text-xl `}>{new Intl.NumberFormat('en-gb', {
+              style: 'currency',
+              currency: 'EUR'
+            }).format(travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier / 100)
+            }</Text>
           </TouchableOpacity>
         )}
        />
-       <View>
+       <View style={tw`mt-auto border-t border-gray-200`}>
         <TouchableOpacity disabled={!selected}
            style={tw`bg-black py-3 m-3 ${!selected && 'bg-gray-300'}`}>
           <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
